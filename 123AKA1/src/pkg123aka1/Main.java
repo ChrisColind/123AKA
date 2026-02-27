d3d9429cb385e0fe5b773bc223202e222e11186d
ChrisColind <cristopher_rogelio@unitec.edu>
2026-02-26 18:53:50 -0600

    primer commit

---
+++ 123AKA1/src/pkg123aka1/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg123aka1;
+
+/**
+ *
+ * @author Rogelio
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+        
+        
+        // TODO code application logic here
+    }
+    
+    
+    
+    
+}
